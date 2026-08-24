05c2a1211968a015d1ff14499c09a9e2e62b1446
Fasdeq13 <wentisoff@gmail.com>
2026-08-24 12:43:31 +0300

    Update client.rs

--- src/platform/ipc/client.rs
+++ src/platform/ipc/client.rs
@@ -5,10 +5,8 @@ use mach2::message::{
     MACH_MSG_TIMEOUT_NONE, MACH_MSG_TYPE_COPY_SEND, MACH_MSG_TYPE_MAKE_SEND, MACH_MSG_TYPE_MOVE_SEND,
     MACH_SEND_MSG,
 };
-use mach2::port::{
-    mach_port_allocate, mach_port_deallocate, mach_port_insert_right, mach_port_mod_refs, mach_port_t,
-    MACH_PORT_NULL, MACH_PORT_RIGHT_RECEIVE,
-};
+use mach2::mach_port::{mach_port_allocate, mach_port_deallocate, mach_port_insert_right, mach_port_mod_refs};
+use mach2::port::{mach_port_t, MACH_PORT_NULL, MACH_PORT_RIGHT_RECEIVE};
 use mach2::traps::mach_task_self;
 use mach2::bootstrap::{bootstrap_look_up, bootstrap_port};
 use std::ffi::CString;
